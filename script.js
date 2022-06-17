3a967aba0720465a6e1711e7724ad9331b94b1c6
IagoBonfim <iagoBonfim@users.noreply.github.com>
2022-06-16 22:15:08 -0300

    Inclusao_do_FOR
    Alterações no código, inclusão de um for para percorrer o Array, e alterações de CSS.

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 function descobrir(){
+    var nome = document.querySelector("div#nome")
     var res = document.querySelector("div#bandeira")
     var vMarca = document.getElementsByName('radmarca')
-    if(vMarca.checked == null){
-        alert("Selecione uma marca para continuar")
-    }else{
-        var imgBandeira = document.createElement('img')
-         imgBandeira.setAttribute('id', 'foto')
+    var imgBandeira = document.createElement('img')
+    imgBandeira.setAttribute('id', 'foto')
+    for(var m of vMarca){
         if(vMarca[0].checked){
             imgBandeira.setAttribute('src', 'img/ferrari.png')
-            res.innerHTML = "Itália!"
+            nome.innerHTML = "Itália!"
+            res.innerHTML = ""
+            res.append(imgBandeira)
         }else if(vMarca[1].checked){
             imgBandeira.setAttribute('src', 'img/fiat.png')
-            res.innerHTML = "Itália!"
+            nome.innerHTML = "Itália!"
+            res.innerHTML = ""
+            res.append(imgBandeira)
         }else if(vMarca[2].checked){
             imgBandeira.setAttribute('src', 'img/ford.png')
-            res.innerHTML = "Estados Unidos!"
+            nome.innerHTML = "Estados Unidos!"
+            res.innerHTML = ""
+            res.append(imgBandeira)
         }else if(vMarca[3].checked){
             imgBandeira.setAttribute('src', 'img/mercedes.png')
-            res.innerHTML = "Alemanha!"
-        }else{
+            nome.innerHTML = "Alemanha!"
+            res.innerHTML = ""
+            res.append(imgBandeira)
+            
+        }else if (vMarca[4].checked){
             imgBandeira.setAttribute('src', 'img/volkswagen.png')
-            res.innerHTML = "Alemanha!"
-
+            nome.innerHTML = "Alemanha!" 
+            res.innerHTML = ""
+            res.append(imgBandeira) 
+           
+        }else{
+            alert("Selecione uma marca para continuar")
+            return
         }
     }
+    
 }
